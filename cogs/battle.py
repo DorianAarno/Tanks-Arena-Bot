7ef4105e8c1f2821d66a2a5a03ea33a9b2e25193
DorianAarno <dorianaarno@gmail.com>
2022-08-02 14:53:31 +0530

    Created test elements

--- cogs/battle.py
+++ cogs/battle.py
@@ -1,11 +1,29 @@
 from disnake import *
 from disnake.ext.commands import *
+from PIL import Image
 
 class Battle(Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    
+    @slash_command()
+    async def battle(self, ctx, opponent: Member):
+        """
+        Parameters
+        ----------
+        opponent: User to battle.
+        """
+
+        # Opening and preparing all the assets
+        background = Image.open('assets/background.png')
+        tank_left = Image.open('assets/tank.png')
+        tank_right = Image.open('assets/tank.png')
+
+        background.paste(tank_left, (0,0))
+        background.paste(tank_right, (background.width,0))
+        background.show()
+
+
 
 def setup(bot):
     bot.add_cog(Battle(bot))
