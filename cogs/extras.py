6836a5144f5418a675b741ebfc5c57db3e5691c8
DorianAarno <dorianaarno@gmail.com>
2022-08-15 12:53:03 +0530

    Added invite command

--- cogs/extras.py
+++ cogs/extras.py
@@ -1,5 +1,5 @@
 from disnake.ext.commands import Cog, slash_command
-
+from disnake import Embed, Color
 
 class Extras(Cog):
     """
@@ -12,6 +12,15 @@ class Extras(Cog):
     @slash_command(name="ping", description="Check the latency of the bot.")
     async def ping(self, ctx):
         await ctx.send(f"📶 {round(self.bot.latency * 1000)}ms")
+
+    @slash_command(description="Invite the bot to other servers.")
+    async def invite(self, ctx):
+        embed = Embed(
+            title="Bot Links",
+            description="[Invite me](https://discord.com/oauth2/authorize?client_id=1003954779346718720&permissions=2147863616&scope=bot%20applications.commands) \n[Support Server](https://discord.gg/D7C4jEkz)",
+            color=Color.blurple()
+        )
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
